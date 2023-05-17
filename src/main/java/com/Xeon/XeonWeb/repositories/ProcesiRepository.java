7f5f1939db533d8e616bb823c99d02b86f42bda1
GledisLami <lamigledi@gmail.com>
2023-05-17 23:13:19 +0200

    Initial commit

--- src/main/java/com/Xeon/XeonWeb/repositories/ProcesiRepository.java
+++ src/main/java/com/Xeon/XeonWeb/repositories/ProcesiRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProcesiRepository extends JpaRepository<Procesi, Integer> {
-    Optional<Procesi> findByProcesi(String procesi);
+    Optional<Procesi> findByPershkrimi(String pershkrimi);
 
     Optional<Procesi> findById(Integer id);
 
